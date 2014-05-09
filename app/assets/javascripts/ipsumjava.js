3527237a86969cb534e98f76af288e7c72c24861
Lucas Mathis <lucas1@lucass-imac.gateway.sonic.net>
2014-05-08 19:08:02 -0700

    basic functionality working

--- app/assets/javascripts/ipsumjava.js
+++ app/assets/javascripts/ipsumjava.js
@@ -2,11 +2,6 @@ $(document).on("click", "#get-lyrics", function(){
   rapper = $("#rapper_id").val()
   numSentences = $("#paragraphs").val()
   numParagraphs = $("#sentences").val()
-  console.log(rapper)
-  console.log(numParagraphs)
-
-  console.log(numSentences)
-
   getLyrics(rapper, numSentences, numParagraphs)
 });
 
@@ -20,13 +15,11 @@ function putLyrics(lyrics) {
 
 function getLyrics(rapper, numSentences, numParagraphs) {
   rapperData = {id: rapper, paras: numParagraphs, sentences: numSentences}
-  console.log(rapperData)
   $.ajax({
     url: '/ipsum',
     data: rapperData,
     type: 'GET'
   }).done(function(lyrics){
-    console.log(lyrics)
     putLyrics(lyrics);
   })
 }
