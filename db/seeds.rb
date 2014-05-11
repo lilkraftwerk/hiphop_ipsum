43089e40e9e515c527dd6402cf86e1105071be79
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-05-11 13:39:27 -0700

    adding ludacris

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-RAPPERS = ["50 Cent", "A Tribe Called Quest", "Beastie Boys", "Chief Keef", "DMX", "Danny Brown", "Dr. Dre", "Gang Starr", "Ghostface", "Juvenile", "Kanye West", "Lil B", "Lil Wayne", "MF Doom", "Notorious B.I.G.", "Outkast", "Slick Rick", "Trick Daddy", "UGK", "Wu-Tang Clan", "Jeezy", "Three 6 Mafia", "Gucci Mane"].sort
+RAPPERS = ["50 Cent", "A Tribe Called Quest", "Beastie Boys", "Chief Keef", "DMX", "Danny Brown", "Dr. Dre", "Gang Starr", "Ghostface", "Juvenile", "Kanye West", "Lil B", "Lil Wayne", "MF Doom", "Notorious B.I.G.", "Outkast", "Slick Rick", "Trick Daddy", "UGK", "Wu-Tang Clan", "Jeezy", "Three 6 Mafia", "Gucci Mane", "Ludacris"].sort
 
 
 RAPPERS.each {|rapper_name| RapperConstructor.new(rapper_name)}
