31730291a501477b2743b71f50e717ef900c0487
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-05-11 13:05:26 -0700

    streamlined seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,4 @@
 RAPPERS = ["50 Cent", "A Tribe Called Quest", "Beastie Boys", "Chief Keef", "DMX", "Danny Brown", "Dr. Dre", "Gang Starr", "Ghostface", "Juvenile", "Kanye West", "Lil B", "Lil Wayne", "MF Doom", "Notorious B.I.G.", "Outkast", "Slick Rick", "Trick Daddy", "UGK", "Wu-Tang Clan", "Jeezy", "Three 6 Mafia", "Gucci Mane"].sort
 
 
-# RAPPERS.each {|rapper_name| RapperConstructor.new(rapper_name)}
-
-RapperConstructor.new(RAPPERS.sample)
+RAPPERS.each {|rapper_name| RapperConstructor.new(rapper_name)}
